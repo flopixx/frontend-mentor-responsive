98bf976ef9d87a85d9900faea89f3c58a280dd66
flopixx <flor.vel96@gmail.com>
2020-11-20 22:10:46 -0300

    proyecto3 responsivo

---
+++ decodifi.js
@@ -0,0 +1,24 @@
+
+
+
+function validar (){
+ let  btn=document.getElementById("ema").value
+ if(btn===""){
+   return alert ("error debe rellenar el campo de email")
+     
+ }else{
+     return console.log(false)
+ }
+} 
+ 
+
+
+
+
+  
+  
+
+
+
+
+
